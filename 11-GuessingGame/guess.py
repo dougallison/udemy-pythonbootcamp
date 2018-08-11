1dbcead952a3a49b69aa30903b74250a665d24c8
Doug Allison <dougallison@deepsky.io>
2018-08-11 09:15:27 -0700

    chore: format guess.py using AutoPep8
    The AutoPep8 plugin formats Python code in accordance with
    the established style guide.
    
    <https://www.python.org/dev/peps/pep-0008/>

--- 11-GuessingGame/guess.py
+++ 11-GuessingGame/guess.py
@@ -4,28 +4,28 @@ random_number = None
 attempts = 0
 best_score = float("inf")
 while(True):
-	if(not random_number):
-		random_number = random.randint(1, 10)
+    if(not random_number):
+        random_number = random.randint(1, 10)
 
-	guess = input('Guess a number between 1 and 10: ')
-	if(not guess):
-		break
+    guess = input('Guess a number between 1 and 10: ')
+    if(not guess):
+        break
 
-	guess = int(guess)
-	attempts += 1
-	if(guess < random_number):
-		print('Too low, try again!')
-	elif(guess > random_number):
-		print('Too high, try again!')
-	else:
-		print(f'You guessed it in {attempts} attempts. You won!')
-		if(attempts < best_score):
-			best_score = attempts
-		play_again = input('Do you want to keep playing? (y/n) ')
-		if(play_again.lower() == 'n'):
-			break;
-		else:
-			random_number = None
-			attempts=0
+    guess = int(guess)
+    attempts += 1
+    if(guess < random_number):
+        print('Too low, try again!')
+    elif(guess > random_number):
+        print('Too high, try again!')
+    else:
+        print(f'You guessed it in {attempts} attempts. You won!')
+        if(attempts < best_score):
+            best_score = attempts
+        play_again = input('Do you want to keep playing? (y/n) ')
+        if(play_again.lower() == 'n'):
+            break
+        else:
+            random_number = None
+            attempts = 0
 
 print(f'Your best score was {best_score} attempts. Thanks for playing!')

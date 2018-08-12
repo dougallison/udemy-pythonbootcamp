fff1b62249eca8163290feb21feb0695d9766265
Doug Allison <dougallison@deepsky.io>
2018-08-11 21:05:05 -0700

    feature: add ex26.py to demonstrate how to create a dictionary.

---
+++ 14-Dictionaries/ex26.py
@@ -0,0 +1,6 @@
+# Create a dictionary called user_info and add at least 3 key value paris to it.
+
+user_info = {'first-name': 'Johannes',
+             'last-name': 'Kepler', 'position': 'Astronomer'}
+
+print(user_info)

181d5bb6b022fd53a1a6799ebbbccdec7c650463
Doug Allison <dougallison@deepsky.io>
2018-08-12 11:13:51 -0700

    feature: add ex29.py to demonstrate dictionary access.
    This solution demonstrates accessing a dictionary with keys
    that may not exist. The `get` method of the dictionary class
    will return a value of `None` if the key does not exist. This is
    a safe approach for accessing a dictionary because it does not
    throw an exception if the key doesn't exist.
    
    This completes exercise 29 from section 14.

---
+++ 14-Dictionaries/ex29.py
@@ -0,0 +1,31 @@
+# Dictionary Access
+#
+# - The food variable will store a randomly chosen food string.
+#   Some of these items are in the bakery_stock dictionary and
+#   some are not.
+# - Print out a string depending on if food is avalue in
+#   bakery_stock. If food is contained in bakery_stock, print
+#   out a string that states how many items are left:
+#   '3 left' for example.
+# - If food is not contained in bakery_stock, print out
+#   'We don't make that.
+
+# This code picks a random food item:
+from random import choice
+food = choice(["cheese pizza", "quiche",
+               "morning bun", "gummy bear", "tea cake"])
+
+bakery_stock = {
+    "almond croissant": 12,
+    "toffee cookie": 3,
+    "morning bun": 1,
+    "chocolate chunk cookie": 9,
+    "tea cake": 25
+}
+
+if(bakery_stock.get(food)):
+    bakery_stock[food] -= 1
+    print(f'{bakery_stock[food]} {food} left')
+    #print('{} left'.format(bakery_stock[food]))
+else:
+    print(f'We don\'t make {food}')
